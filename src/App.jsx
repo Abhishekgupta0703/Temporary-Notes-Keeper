189882afd8cf6be17e5d5583252edf4831976cec
Abhishek Gupta <114281145+Abhishekgupta0703@users.noreply.github.com>
2023-07-02 23:31:09 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Foods from "./Foods";
+function App() {
+  return (
+    <div className="App">
+      <h1>Favorito Foods</h1>
+      <Foods />
+    </div>
+  );
+}
+export default App;
---
+++ src/Foods.jsx
@@ -0,0 +1,9 @@
+function Foods() {
+  const foods = ["Kheer", "Rayta", "Paneer"];
+  return (
+    <>
+      <p></p>
+    </>
+  );
+}
+export default Foods;
